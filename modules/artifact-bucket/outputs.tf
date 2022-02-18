e99ecbfd247e9d5840a4061f98dd57297141a38b
Joe Ferris <jferris@thoughtbot.com>
2022-02-18 15:56:50 -0500

    Remove modules duplicated by codestar-setup
    https://github.com/thoughtbot/terraform-codestar-setup

--- modules/artifact-bucket/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "arn" {
-  description = "ARN of the created bucket"
-  value       = aws_s3_bucket.this.arn
-}
-
-output "name" {
-  description = "Name of the created bucket"
-  value       = aws_s3_bucket.this.bucket
-}
--- modules/codebuild-credential/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "arn" {
-  description = "ARN of the created credential"
-  value       = aws_codebuild_source_credential.this.arn
-}
