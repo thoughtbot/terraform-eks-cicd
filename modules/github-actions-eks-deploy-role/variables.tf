6ce0f142a9575d18a361ddec0e6c2fe097438c46
Olamide <olamide@thoughtbot.com>
2022-07-05 16:55:31 +0100

    Add IAM role to deploy to EKS

---
+++ modules/github-actions-eks-deploy-role/variables.tf
@@ -0,0 +1,30 @@
+variable "cluster_name" {
+  type        = string
+  description = "Name of the EKS cluster"
+}
+
+variable "github_branches" {
+  description = "Branches allowed to push to this repository"
+  type        = list(string)
+}
+
+variable "github_organization" {
+  description = "Name of the GitHub organization which will assume this role"
+  type        = string
+}
+
+variable "github_repository" {
+  description = "Name of the GitHub repository which will assume this role"
+  type        = string
+}
+
+variable "iam_oidc_provider_arn" {
+  description = "ARN of the IAM OIDC provider for GitHub"
+  type        = string
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to be applied to created AWS resources"
+  default     = {}
+}
