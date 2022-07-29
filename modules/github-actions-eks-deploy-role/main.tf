23ec5baaee0148d01d44fad60ad6e719c12020a4
Chris K <chrisk@thoughtbot.com>
2022-07-29 10:11:45 -0700

    allow eks deploy role access to all clusters

--- modules/github-actions-eks-deploy-role/main.tf
+++ modules/github-actions-eks-deploy-role/main.tf
@@ -32,7 +32,7 @@ data "aws_iam_policy_document" "eks" {
   statement {
     effect    = "Allow"
     actions   = ["eks:DescribeCluster"]
-    resources = [data.aws_eks_cluster.this.arn]
+    resources = ["*"]
   }
 }
 
